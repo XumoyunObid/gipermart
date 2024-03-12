4fd83a082d366bc43a902e45e0c12428027908ce
Xumoyun <obidjonov06122005@gmail.com>
2024-03-12 17:15:53 +0500

    final commit

--- src/pages/Products/SingleProduct.jsx
+++ src/pages/Products/SingleProduct.jsx
@@ -34,31 +34,6 @@ const SingleProduct = () => {
   };
   return (
     <div>
-      <div className="shadow-xl">
-        <ul className="container py-3 flex items-center justify-between">
-          <li>
-            <Link>Смартфоны и планшеты</Link>
-          </li>
-          <li>
-            <Link>Ноутбуки, планшеты и компьютеры</Link>
-          </li>
-          <li>
-            <Link>Техника для дома</Link>
-          </li>
-          <li>
-            <Link>Игры и развлечения</Link>
-          </li>
-          <li>
-            <Link>Телевизоры, Аудио-видео, Hi-Fi</Link>
-          </li>
-          <li>
-            <Link>Фото и видеотехника</Link>
-          </li>
-          <li>
-            <Link>Еще</Link>
-          </li>
-        </ul>
-      </div>
       <div className="container py-6">
         <h1 className="text-3xl font-semibold">{product?.title}</h1>
         <div className="flex gap-9 items-center py-4">
@@ -82,55 +57,63 @@ const SingleProduct = () => {
           </div>
           {product ? (
             <div className="w-1/3 items-start">
-              <div className="flex flex-col gap-2 items-start">
-                <p className="text-base font-medium">Объем памяти</p>
-                <span className="px-3 py-2 border border-red-600 text-base">
-                  {product?.rom}GB
-                </span>
-              </div>
-              <div>
-                <h1 className="text-lg font-medium py-4">Характеристики</h1>
-                <ul className="flex flex-col gap-3">
-                  <li>
-                    <span className="text-base text-gray-500">Цвет:</span>{" "}
-                    {product?.color}
-                  </li>
-                  <li>
-                    <span className="text-base text-gray-500">Экран:</span>{" "}
-                    {product?.screen}
-                  </li>
-                  <li>
-                    <span className="text-base text-gray-500">
-                      Оперативная память:
-                    </span>{" "}
-                    {product?.ram}GB
-                  </li>
-                  <li>
-                    <span className="text-base text-gray-500">Память:</span>{" "}
+              {product?.ram ? (
+                <div className="flex flex-col gap-2 items-start">
+                  <p className="text-base font-medium">Объем памяти</p>
+                  <span className="px-3 py-2 border border-red-600 text-base">
                     {product?.rom}GB
-                  </li>
-                  {product?.cpu ? (
+                  </span>
+                </div>
+              ) : (
+                ""
+              )}
+              {product?.color ? (
+                <div>
+                  <h1 className="text-lg font-medium py-4">Характеристики</h1>
+                  <ul className="flex flex-col gap-3">
+                    <li>
+                      <span className="text-base text-gray-500">Цвет:</span>{" "}
+                      {product?.color}
+                    </li>
+                    <li>
+                      <span className="text-base text-gray-500">Экран:</span>{" "}
+                      {product?.screen}
+                    </li>
                     <li>
                       <span className="text-base text-gray-500">
-                        Процессор:
+                        Оперативная память:
                       </span>{" "}
-                      {product?.cpu}
+                      {product?.ram}GB
                     </li>
-                  ) : (
-                    ""
-                  )}
-                  {product?.screentype ? (
                     <li>
-                      <span className="text-base text-gray-500">
-                        Тип матрицы экрана:
-                      </span>{" "}
-                      {product?.screentype}
+                      <span className="text-base text-gray-500">Память:</span>{" "}
+                      {product?.rom}GB
                     </li>
-                  ) : (
-                    ""
-                  )}
-                </ul>
-              </div>
+                    {product?.cpu ? (
+                      <li>
+                        <span className="text-base text-gray-500">
+                          Процессор:
+                        </span>{" "}
+                        {product?.cpu}
+                      </li>
+                    ) : (
+                      ""
+                    )}
+                    {product?.screentype ? (
+                      <li>
+                        <span className="text-base text-gray-500">
+                          Тип матрицы экрана:
+                        </span>{" "}
+                        {product?.screentype}
+                      </li>
+                    ) : (
+                      ""
+                    )}
+                  </ul>
+                </div>
+              ) : (
+                ""
+              )}
             </div>
           ) : (
             ""
